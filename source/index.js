ee7100e23b40d1810cc4a2a318c562c8d3ccb7ca
Andrew McPherson <andrewmcp333@gmail.com>
2015-08-21 21:09:20 -0700

    Restructured most of the auto-generated code.

--- source/index.js
+++ source/index.js
@@ -1,4 +1,15 @@
-window.React = require("react/addons")
+window.React = require("react")
 
-var Game = require("<scripts>/components/Game")
+var GameFrame = require("<scripts>/views/GameFrame")
+
+var Game = React.createClass({
+    render: function() {
+        return (
+            <GameFrame>
+                Hello World!!
+            </GameFrame>
+        )
+    }
+})
+
 React.render(<Game/>, document.body)
